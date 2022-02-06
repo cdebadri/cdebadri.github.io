9186baf206d3cf48efab98db9f8f42e1493e929a
cdebadri <cdebadri@gmail.com>
2022-02-06 15:57:42 +0530

    build changes

--- README.md
+++
@@ -1,1 +0,0 @@
-# spacebattle-v2
--- src/components/CleanupGroup.js
+++
@@ -1,9 +0,0 @@
-import * as Phaser from 'phaser';
-
-export default function(group, scene) {
-  group.children.each(function(child) {
-    if (!Phaser.Geom.Rectangle.Overlaps(scene.physics.world.bounds, child.getBounds())) {
-      child.destroy();
-    }
-  });
-}
